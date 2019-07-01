5757f0e01a3de58fe5d3d926c0a2905eeb2b77c3
savannah8 <willengitonga.wg@gmail.com>
2019-07-01 07:47:35 +0300

    added serializers to convert django models to JSON

--- awardapp/serializer.py
+++ awardapp/serializer.py
@@ -4,9 +4,9 @@ from .models import Profile,Project
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('user', 'bio','profile_photo')
+        fields = ('id','user', 'bio','profile_photo')
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('user', 'project_name','project_url','image' )
+        fields = ('id','user', 'project_name','project_url','image' )
